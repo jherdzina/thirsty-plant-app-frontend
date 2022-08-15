e37fb8e6321887268e593986dc383c69fde340d5
Jennifer Herdzina <92278996+jherdzina@users.noreply.github.com>
2022-08-15 12:38:44 -0500

    turn off cors issue, update req url for FE to BE connection

--- src/api/index.js
+++ src/api/index.js
@@ -1,17 +1,19 @@
-import axios from 'axios';
-
-const API = axios.create({ baseURL: 'http://localhost:5001', headers: {'Access-Control-Allow-Origin': '*'} });
+//import axios from 'axios';
+// const API = axios.create({ baseURL: 'http://localhost:5001', headers: {'Access-Control-Allow-Origin': '*'} });
 
 // export const testPost = () => API.get('/plants');
 
 export function testPost() {
-    let url = 'http://localhost:5001/plants'
-    fetch(url, {
-        method: 'GET',
-        headers:{
-          'Access-Control-Allow-Origin': '*'
-        }
-      }).then(res => res.json())
-      .then(response => console.log('Success:', response))
-      .catch(error => console.error('Error:', error));
-}
+    let url = 'https://afternoon-savannah-22384.herokuapp.com/plants';
+    var response = fetch(url, {
+      method: 'GET',
+      mode: 'no-cors',
+      headers:{
+        'Access-Control-Allow-Origin': '*',
+        "Access-Control-Allow-Headers": "*, Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers",
+        "Access-Control-Allow-Credentials": "true",
+        'Content-Type': 'application/json'
+      }
+    }).then(res => res.text()).then(response => console.log('Success:', response)).catch(error => console.error('Error:', error));
+    console.log(response)
+  }
