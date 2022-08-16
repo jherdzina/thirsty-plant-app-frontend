c382fe9de0f0ed1e33d417d0d095ee94d88a893e
Jennifer Herdzina <92278996+jherdzina@users.noreply.github.com>
2022-08-15 20:42:03 -0500

    fix build

--- src/api/index.js
+++ src/api/index.js
@@ -10,10 +10,10 @@ export function testPost(plant) {
     var response = fetch(url, {
       method: 'GET',
       headers:{
-        'Access-Control-Allow-Origin': '*',
-        "Access-Control-Allow-Headers": "*, Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers",
-        "Access-Control-Allow-Credentials": "true",
-        'Content-Type': 'application/json'
+        'Access-Control-Allow-Origin': '*'
+        // "Access-Control-Allow-Headers": "*, Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers",
+        // "Access-Control-Allow-Credentials": "true",
+        // 'Content-Type': 'application/json'
       }
     }).then(res => res.text()).then(res => console.log('Success:', d)).catch(error => console.error('Error:', error));
     console.log(response + d)
