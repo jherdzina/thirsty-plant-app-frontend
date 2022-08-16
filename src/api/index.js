6bca9eb21a0c2d01f2529a551de8f3d6f7d78653
Jennifer Herdzina <92278996+jherdzina@users.noreply.github.com>
2022-08-15 20:59:13 -0500

    fix build

--- src/api/index.js
+++ src/api/index.js
@@ -9,11 +9,12 @@ export function testPost(plant) {
     let url = 'https://afternoon-savannah-22384.herokuapp.com/plants';
     var response = fetch(url, {
       method: 'GET',
+      mode: 'no-cors',
       headers:{
-        'Access-Control-Allow-Origin': '*'
-        // "Access-Control-Allow-Headers": "*, Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers",
-        // "Access-Control-Allow-Credentials": "true",
-        // 'Content-Type': 'application/json'
+        'Access-Control-Allow-Origin': '*',
+        "Access-Control-Allow-Headers": "*, Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers",
+        "Access-Control-Allow-Credentials": "true",
+        'Content-Type': 'application/json'
       }
     }).then(res => res.text()).then(res => console.log('Success:', d)).catch(error => console.error('Error:', error));
     console.log(response + d)
